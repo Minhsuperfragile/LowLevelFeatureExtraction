016c480edc9da7d99252427822d90b4523864c41
TrongMinh <trongminhmcw10e@gmail.com>
2025-02-14 01:15:32 +0700

    now trainable, but nan loss T_T

--- train.py
+++ train.py
@@ -1,15 +1,42 @@
-import pyfeats
-from typing import *
-from torchvision import datasets, transforms
+from torchvision import transforms
 from torch.utils.data import DataLoader
 import os
-
-
 from utils import *
 
-llf = LowLevelFeatureExtractor(function=pyfeats.glcm_features, 
-                               params={'ignore_zeros': True}, 
-                               features_set=['features_mean', 'features_range'])
+ckpt_folder = "./ckpts/"
+# Check if the directory already exists
+if not os.path.exists(ckpt_folder):
+    # Create the directory using mkdir()
+    os.makedirs(ckpt_folder)
+    print(f"Folder '{ckpt_folder}' created.")
 
-root_folder = "/mnt/c/Users/trong/Documents/skin_data"
+# root_folder = "../skin_data/"
+root_folder = "/mnt/c/Users/trong/Documents/skin_data" 
 
+# Define image transformations
+transform = transforms.Compose([
+    transforms.Resize((128, 128)),  # Resize images to 128x128
+    transforms.Grayscale(num_output_channels=1),    # Convert to grayscale,
+    transforms.ToTensor()  # Convert to tensor
+])
+
+train_dataset = CSVImageMetadataDataset(csv_file='./data/vaynen_train_linux.csv', root_dir=root_folder, transform=transform)
+train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4)   
+
+test_dataset = CSVImageMetadataDataset(csv_file='./data/vaynen_test_linux.csv', root_dir=root_folder, transform=transform)
+test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=4)
+
+for llf_param_set in param_list[:1]:
+    llf = LowLevelFeatureExtractor(**llf_param_set)
+
+    llf_name = llf.function.__name__
+
+    model = SimpleNeuralNetwork(inputs= llf.get_features_size())
+
+    train_model(model, train_dataloader, llf, 3, llf_name)
+    result = evaluate_model(model, test_dataloader, llf, llf_name)
+
+    with open("./ckpts/results.txt", "a") as f:
+        f.write(f"{result}\n")
+
+    torch.save(model.state_dict(), f'./ckpts/model_{llf_name}.pth')
--- utils.py
+++ utils.py
@@ -7,6 +7,155 @@ from PIL import Image
 import pandas as pd
 import os
 from tqdm import tqdm
+import torchvision
+
+mask = None
+param_list = [
+{
+    'function': pyfeats.fos, #First Order Statistics/Statistical Features (FOS/SF)
+    'params': {'mask': mask },
+    'features_set': ['features']
+},
+{
+    "function": pyfeats.glcm_features, #Gray Level Co-occurence Matrix (GLCM/SGLDM)
+    "params": {'ignore_zeros': True},
+    "features_set": ['features_mean', 'features_range']
+}, 
+{
+    "function": pyfeats.glds_features, #Gray Level Difference Statistics (GLDS)
+    "params": {'mask': mask, 'Dx': [0,1,1,1], 'Dy': [1,1,0,-1]},
+    "features_set": ['features']
+},
+{
+    "function": pyfeats.ngtdm_features, #Neighborhood Gray Tone Difference Matrix (NGTDM)
+    "params": {'mask': mask, 'd': 1},
+    "features_set": ['features']
+},
+{
+    "function": pyfeats.sfm_features, #Statistical Feature Matrix (SFM)
+    'params': {'mask': mask, 'Lr':4, 'Lc':4},
+    'features_set': ['features']
+},
+{
+    "function": pyfeats.lte_measures, #Law's Texture Energy Measures (LTE/TEM)
+    'params': {'mask': mask, 'l': 7},
+    'features_set': ['features']
+},
+{
+    "function": pyfeats.fdta, #Fractal Dimension Texture Analysis (FDTA)
+    'params': {'mask': mask, 's':3},
+    'features_set': ['features']
+},
+{
+    "function": pyfeats.glrlm_features, #Gray Level Run Length Matrix (GLRLM)
+    'params': {'mask': mask, 'Ng': 256},
+    'features_set': ['features']
+},
+{
+    "function": pyfeats.fps, #Fourier Power Spectrum (FPS)
+    'params': {'mask': mask},
+    'features_set': ['features']
+},
+{
+    "function": pyfeats.shape_parameters, #Shape Parameters
+    'params': {'mask': mask, 'perimeter': np.ones((128,128)), 'pixels_per_mm2':1},
+    'features_set': ['features']
+},
+{
+    "function": pyfeats.glszm_features, #Gray Level Size Zone Matrix (GLSZM)
+    'params': {'mask': mask},
+    'features_set': ['features']
+},
+{
+    "function": pyfeats.hos_features, #Higher Order Spectra (HOS)
+    'params': {'th': [135,140]},
+    'features_set': ['features']
+},
+{
+    "function": pyfeats.lbp_features, #Local Binary Pattern (LPB)
+    'params': {'mask': mask,  'P':[8,16,24], 'R':[1,2,3]}, 
+    'features_set': ['features']
+},
+{
+    "function": pyfeats.grayscale_morphology_features, #Gray-scale Morphological Analysis
+    'params': {"N": 30},
+    'features_set': ['pdf', 'cdf']
+},
+# { Deprecated: generate NaN values in the output}
+#     "function": pyfeats.multilevel_binary_morphology_features, #Multilevel Binary Morphological Analysis
+#     'params': {'mask': mask, 'N': 30, 'thresholds': [25, 50]},
+#     'features_set': ['pdf_L', 'pdf_M', 'pdf_H', 'cdf_L', 'cdf_M', 'cdf_H']
+# },
+{
+    'function': pyfeats.histogram, #Histogram
+    'params': {'mask': mask, 'bins': 32},
+    'features_set': ['H']
+},
+{
+    "function": pyfeats.multiregion_histogram, #Multi-region histogram
+    'params': {'mask': mask, 'bins': 32, 'num_eros': 3, 'square_size': 3},
+    'features_set': ['features']
+},
+# { Deprecated: generate all 0 values in the output}
+#     "function": pyfeats.correlogram, #Correlogram
+#     'params': {'mask': mask, 'bins_digitize' : 32, 'bins_hist' : 32, 'flatten' : True},
+#     'features_set': ['Hd', 'Ht']
+# },
+{
+    "function": pyfeats.fdta, #Fractal Dimension Texture Analysis (FDTA)
+    'params': {'mask': mask, 's': 3},
+    'features_set': ['h']
+},
+{
+    "function": pyfeats.amfm_features, #Amplitude Modulation – Frequency Modulation (AM-FM)
+    'params': {'bins': 32},
+    'features_set': ['features'] # Take long time to calculate
+},
+{
+    "function": pyfeats.dwt_features, #Discrete Wavelet Transform (DWT)
+    'params': {'mask': mask, 'wavelet': 'bior3.3', 'levels': 3},
+    'features_set': ['features']
+},
+{
+    "function": pyfeats.swt_features, #Stationary Wavelet Transform (SWT)
+    'params': {'mask': mask, 'wavelet': 'bior3.3', 'levels': 3},
+    'features_set': ['features']
+},
+{
+    "function": pyfeats.wp_features, #Wavelet Packets (WP)
+    'params': {'mask': mask, 'wavelet': 'coif1', 'maxlevel': 3},
+    'features_set': ['features']
+},
+{
+    "function": pyfeats.gt_features, #Gabor Transform (GT)
+    'params': {'mask': mask, 'deg': 4, 'freq': [0.05, 0.4]},
+    'features_set': ['features']
+},
+{
+    "function": pyfeats.zernikes_moments, #Zernikes’ Moments
+    'params': {'radius': 9},
+    'features_set': ['features']
+},
+{
+    "function": pyfeats.hu_moments, #Hu’s Moments
+    'params': {},
+    'features_set': ['features']
+},
+{
+    "function": pyfeats.tas_features, #Threshold Adjacency Matrix (TAS)
+    'params': {},
+    'features_set': ['features']
+},
+{
+    "function": pyfeats.hog_features, #Histogram of Oriented Gradients (HOG)
+    'params': {'ppc': 8, 'cpb': 3},
+    'features_set': ['features']
+}
+]
+
+class ToNumpy():
+    def __call__(self, *args, **kwds):
+        return np.array(args[0])
 
 class LowLevelFeatureExtractor:
     def __init__(self, function: Callable, # pyfeats function to call
@@ -17,23 +166,30 @@ class LowLevelFeatureExtractor:
         self.params = params if params is not None else {}
         self.features_set = features_set
 
-    def __call__(self, image):
-        self.params['f'] = np.array(image)
+    def __call__(self, images):
+        images = np.array(images) if not isinstance(images, np.ndarray) else images
+        features = []
 
-        features_output = self.function(**self.params)
-        
-        features_set = {feature: value for feature, value in zip(self.features_set, features_output)}
+        for image in images:
+            image= np.squeeze(image)
 
-        features_set = np.concatenate([features_set[key] for key in features_set.keys()], axis=0)
+            features_output = self.function(image, **self.params)
+            
+            features_set = {feature: value for feature, value in zip(self.features_set, features_output)}
+
+            features_set = np.concatenate([features_set[key] for key in features_set.keys()], axis=0)
+            # features.append(np.expand_dims(features_set, axis=0))
+            features.append(features_set)
 
         self.features_size = features_set.shape[0]
 
-        return features_set
+        return np.stack(features, axis=0)
     
     def get_features_size(self) -> int:
         sample_image = np.random.randint(0, 256, (32, 32))
-        features_set = self(sample_image)
-        self.features_size = features_set.shape[0]
+        features_set = self(np.expand_dims(sample_image, axis=0))
+
+        self.features_size = features_set.shape[1]
 
         return self.features_size
 
@@ -76,19 +232,20 @@ class SimpleNeuralNetwork(torch.nn.Module):
         self.swish = SwishActivation()
         self.batchNorm = torch.nn.BatchNorm1d(64)
         self.dropout = torch.nn.Dropout(p=0.3)
+        self.sigmoid = torch.nn.Sigmoid()
 
     def forward(self, x, metadata):
         x = torch.cat((x, metadata), dim=1).float()
         x = self.fc1(x)
         x = self.batchNorm(x)
-        x = self.swish(x)
+        x = self.sigmoid(x)
         x = self.dropout(x)
 
         x = self.fc2(x)
         # x = self.softmax(x)
         return x
 
-def train_model(model: SimpleNeuralNetwork, train_loader: torch.utils.data.DataLoader, epochs: int, features_set: str):
+def train_model(model: SimpleNeuralNetwork, train_loader: torch.utils.data.DataLoader, llf: LowLevelFeatureExtractor, epochs: int, features_set: str):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
     model.train()
@@ -101,8 +258,12 @@ def train_model(model: SimpleNeuralNetwork, train_loader: torch.utils.data.DataL
         running_loss = 0.0
         for inputs, metadata, labels in train_loader:
             optimizer.zero_grad()
+            inputs = torchvision.transforms.ToTensor(llf(inputs)).to(device)
+            labels = labels.long().to(device)
+            metadata = metadata.to(device)
+
             outputs = model(inputs, metadata)
-            loss = criterion(outputs, labels.long())
+            loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
@@ -110,17 +271,23 @@ def train_model(model: SimpleNeuralNetwork, train_loader: torch.utils.data.DataL
 
     print(f"Training on {features_set} complete!")
 
-def evaluate_model(model: SimpleNeuralNetwork, test_loader: torch.utils.data.DataLoader, features_set = str):
+def evaluate_model(model: SimpleNeuralNetwork, test_loader: torch.utils.data.DataLoader, llf: LowLevelFeatureExtractor, features_set = str):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
     model.eval()
+
     correct = 0
     total = 0
     with torch.no_grad():
         for inputs, metadata, labels in test_loader:
+            inputs = torch.Tensor(llf(inputs)).to(device)
+            labels = labels.long().to(device)
+            metadata = metadata.to(device)
             outputs = model(inputs, metadata)
             _, predicted = torch.max(outputs.data, 1)
 
             total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+            correct += (predicted.to(device) == labels).sum().item()
 
     return f"Accuracy: {100 * correct / total}% On {features_set}"
 
