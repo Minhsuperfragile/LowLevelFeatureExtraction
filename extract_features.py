00b6451a041b62b5ae15cf15a4e6804a5636615b
TrongMinh <trongminhmcw10e@gmail.com>
2025-02-19 09:55:53 +0700

    update result

--- extract_features.py
+++ extract_features.py
@@ -7,14 +7,16 @@ train_df = pd.read_csv("./data/linux/vaynen_train_linux.csv", delimiter=',')
 test_df = pd.read_csv("./data/linux/vaynen_test_linux.csv", delimiter=',')
 root_folder = "../../skin_data/"
 
+image_size_normalize = (384,384)
+
 transform = transforms.Compose([
-    transforms.Resize((386,386)),  # Resize images to 128x128
+    transforms.Resize(image_size_normalize),  # Resize images to 128x128
     transforms.Grayscale(num_output_channels=1),    # Convert to grayscale
     ToNumpy(),  # Convert to tensor
 ])
 
 for llf_param in param_list[22:]:
-    llf = LowLevelFeatureExtractor(**llf_param)
+    llf = LowLevelFeatureExtractor(**llf_param, image_size=image_size_normalize)
 
     llf_name = llf.function.__name__
     process_dataframe(test_df, llf, transform, root_folder, save_path=f"./data/{llf_name}/vaynen_test_{llf_name}.csv")
--- train_neural_network.py
+++ train_neural_network.py
@@ -14,6 +14,8 @@ if not os.path.exists(ckpt_folder):
 
 batch_size = 32
 num_workers = 4
+md = "nmd"
+result_df = pd.read_csv("./ckpts/multiple_result.csv", index_col="features_name")
 
 for llf_param_set in param_list:
     llf = LowLevelFeatureExtractor(**llf_param_set)
@@ -23,20 +25,22 @@ for llf_param_set in param_list:
     if not os.path.exists(os.path.join(csv_folder, llf_name)):
         continue
 
-    train_csv = os.path.join(csv_folder, llf_name, f"vaynen_train_{llf_name}.csv")
-    test_csv = os.path.join(csv_folder, llf_name, f"vaynen_test_{llf_name}.csv")
+    train_csv = os.path.join(csv_folder, llf_name, f"vaynen_train_{llf_name}_new.csv")
+    test_csv = os.path.join(csv_folder, llf_name, f"vaynen_test_{llf_name}_new.csv")
 
-    train_dataset = CSVMetadataDataset(csv_file=train_csv, root_dir=root_folder)
-    test_dataset = CSVMetadataDataset(csv_file=test_csv, root_dir=root_folder)
+    train_dataset = CSVMetadataDataset(csv_file=train_csv, root_dir=root_folder, md=(md=="md"))
+    test_dataset = CSVMetadataDataset(csv_file=test_csv, root_dir=root_folder, md=(md=="md"))
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
-    model = SimpleNeuralNetwork(inputs= llf.get_features_size())
+    model = SimpleNeuralNetwork(inputs= train_dataset.get_input_size(), classes=2)
 
     train_model(model, train_dataloader, llf, 30, llf_name)
-    result = evaluate_model(model, test_dataloader, llf, llf_name)
+    result , cm = evaluate_model(model, test_dataloader, llf, llf_name)
 
-    with open("./ckpts/results.txt", "a") as f:
-        f.write(f"{result}\n")
+    result_df.at[llf_name, f"nn_{md}"] = result
+    result_df.at[f'{llf_name}_0', f"nn_{md}"] = cm[0]
+    result_df.at[f'{llf_name}_1', f"nn_{md}"] = cm[1]
+    # torch.save(model.state_dict(), f'./ckpts/model_{llf_name}.pth')
 
-    torch.save(model.state_dict(), f'./ckpts/model_{llf_name}.pth')
+result_df.to_csv("./ckpts/multiple_result.csv")
